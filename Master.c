e6ffc08a8ea83a6edaf257e06fd99ea2b1791129
Lorenzo <cassinelli.lorenzo00@gmail.com>
2020-12-15 11:20:38 +0100

    Modifiche lettura file

--- Master.c
+++ Master.c
@@ -23,8 +23,9 @@ void lettura_file();
 int main(void)
 {
     char deallocator[50] = "";
-    /*lettura_file();*/
+    lettura_file();
     generateMap(7,3,3);
+    
     /* TODO: write deallocator function */
     /*
 
@@ -36,40 +37,55 @@ int main(void)
 
     */
 
-
-
   exit(EXIT_SUCCESS);
 }
 
 void lettura_file(){
-  int i;
   FILE* configFile;
   char* path = "./Input.config";
-  char string[15];
-  int parametri[10];
+  char string[20];
   int value;
-
-  i=0;
-
+  
   if((configFile=fopen(path, "r"))==NULL) {
 	printf("Errore nell'apertura del file'");
 	exit(1);
   }
   while(!feof(configFile)){
+
   	fscanf(configFile,"%s %d",string,&value);
-  	parametri[i]=value;
-  	i++;
+  	
+  	if(strcmp(string,"SO_TAXI")==0){
+  		SO_TAXI=value;
+  	}
+  	else if(strcmp(string,"SO_SOURCES")==0){
+  		SO_SOURCES=value;
+  	}
+  	else if(strcmp(string,"SO_HOLES")==0){
+  		SO_HOLES=value;
+  	}
+  	else if(strcmp(string,"SO_TOP_CELLS")==0){
+  		SO_TOP_CELLS=value;
+  	}
+  	else if(strcmp(string,"SO_CAP_MIN")==0){
+  		SO_CAP_MIN=value;
+  	}
+  	else if(strcmp(string,"SO_CAP_MAX")==0){
+  		SO_CAP_MAX=value;
+  	}
+  	else if(strcmp(string,"SO_TIMENSEC_MIN")==0){
+  		SO_TIMENSEC_MIN=value;
+  	}
+  	else if(strcmp(string,"SO_TIMENSEC_MAX")==0){
+  		SO_TIMENSEC_MAX=value;
+  	}
+  	else if(strcmp(string,"SO_TIMEOUT")==0){
+  		SO_TIMEOUT=value;
+  	}
+  	else if(strcmp(string,"SO_DURATION")==0){
+  		SO_DURATION=value;
+  	}
+  	else
+  	    printf("Stringa letta non è presente come parametro nel testo\n");
   }
   fclose(configFile);
-
-  SO_TAXI=parametri[0];
-  SO_SOURCES=parametri[1];
-  SO_TOP_CELLS=parametri[2];
-  SO_CAP_MIN=parametri[3];
-  SO_CAP_MAX=parametri[4];
-  SO_TIMENSEC_MIN=parametri[5];
-  SO_TIMENSEC_MAX=parametri[6];
-  SO_TIMEOUT= parametri[7];
-  SO_DURATION=parametri[8];
-
 }
